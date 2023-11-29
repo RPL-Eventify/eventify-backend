5c70162a99df6ed595205413da2eb3f40166cf33
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2023-11-29 21:39:57 +0700

    implement acara API for current user

--- apps/para/urls.py
+++ apps/para/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('aktivitas/', views.AktivitasView.as_view(), name='aktivitas-list'),
+    path('acara/', views.AcaraView.as_view(), name='acara-list'),
 ]
--- apps/para/views.py
+++ apps/para/views.py
@@ -2,8 +2,8 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
-from .models import Aktivitas
-from .serializers import AktivitasSerializer
+from .models import Aktivitas, Acara
+from .serializers import AktivitasSerializer, AcaraSerializer
 
 
 # Create your views here.
@@ -18,4 +18,18 @@ class AktivitasView(ListCreateAPIView):
         return Aktivitas.objects.filter(pemilik=user)
 
     def perform_create(self, serializer):
+        serializer.save(pemilik=self.request.user)
+
+class AcaraView(ListCreateAPIView):
+    serializer_class = AcaraSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['kategori']
+
+    def get_queryset(self):
+        user = self.request.user
+        return Acara.objects.filter(pemilik=user)
+
+    def perform_create(self, serializer):
+        serializer.validated_data['kategori'] = 'projects'
         serializer.save(pemilik=self.request.user)
