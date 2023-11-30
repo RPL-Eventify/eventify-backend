8f8f74244f8b70257beea175790f5857ff370c9d
Hafiz <hafizadhyaulhaq16@gmail.com>
2023-11-30 23:24:20 +0700

    Implement API Get Single Aktivitas

--- apps/para/views.py
+++ apps/para/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework import serializers
+from rest_framework.exceptions import NotFound
 from .models import Aktivitas, Acara
+from .permission import IsCurrentUserOrReadOnly
 from .serializers import AktivitasSerializer, AcaraSerializer
 
 
@@ -33,3 +35,16 @@ class AcaraView(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.validated_data['kategori'] = 'projects'
         serializer.save(pemilik=self.request.user)
+
+class AktivitasDetailView(RetrieveAPIView):
+    serializer_class = AktivitasSerializer
+    permission_classes = [IsAuthenticated, IsCurrentUserOrReadOnly]
+
+    def get_object(self):
+        id_param = self.kwargs.get('id')
+        try:
+            aktivitas = Aktivitas.objects.get(id=id_param)
+            self.check_object_permissions(self.request, aktivitas)
+            return aktivitas
+        except Aktivitas.DoesNotExist:
+            raise NotFound(f"Aktivitas dengan id {id_param} tidak ditemukan", code="Activity_Not_Found")
