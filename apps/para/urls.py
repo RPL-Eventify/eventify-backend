9447d1e29a56533ff366485b2d9b3d330e207bcd
Rahmat Bryan Naufal <rahmat.bryan@ui.ac.id>
2023-12-03 11:58:27 +0700

    implement archive unarchive delete

--- apps/para/urls.py
+++ apps/para/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('acara/', views.AcaraView.as_view(), name='acara-list'),
     path('aktivitas/<uuid:id>/', views.AktivitasDetailView.as_view(), name='aktivitas-detail'),
     path('acara/<uuid:id>/', views.AcaraDetailView.as_view(), name='acara-detail'),
+    path('archive/<uuid:id>/', views.ArchiveKegiatanView.as_view(), name='archive-kegiatan'),
+    path('unarchive/<uuid:id>/', views.UnarchiveKegiatanView.as_view(), name='unarchive-kegiatan'),
+    path('delete/<uuid:id>/', views.DeleteKegiatanView.as_view(), name='unarchive-kegiatan'),
 ]
--- apps/para/views.py
+++ apps/para/views.py
@@ -1,11 +1,14 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView ,get_object_or_404
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.exceptions import NotFound
-from .models import Aktivitas, Acara
+from .models import Aktivitas, Acara, KegiatanPARA, KegiatanPARAManager
 from .permission import IsCurrentUserOrReadOnly
-from .serializers import AktivitasSerializer, AcaraSerializer
+from .serializers import AktivitasSerializer, AcaraSerializer, KegiatanPARASerializer
 
 
 # Create your views here.
@@ -62,3 +65,55 @@ class AcaraDetailView(RetrieveAPIView):
             return acara
         except Acara.DoesNotExist:
             raise NotFound(f"Acara dengan id {id_param} tidak ditemukan", code="Acara_Not_Found")
+
+class ArchiveKegiatanView(APIView):
+    permission_classes = [IsAuthenticated, IsCurrentUserOrReadOnly]
+
+    def post(self, request, id):
+        kegiatan = self.get_kegiatan(id)
+        kegiatan.archived()
+        return Response({'status': 'Kegiatan archived successfully'}, status=status.HTTP_200_OK)
+
+    def get_kegiatan(self, id):
+        try:
+            return Acara.objects.get(id=id)
+        except Acara.DoesNotExist:
+            try:
+                return Aktivitas.objects.get(id=id)
+            except Aktivitas.DoesNotExist:
+                raise NotFound(f"Kegiatan dengan id {id} tidak ditemukan", code="Kegiatan_Not_Found")
+
+
+class UnarchiveKegiatanView(APIView):
+    permission_classes = [IsAuthenticated, IsCurrentUserOrReadOnly]
+
+    def post(self, request, id):
+        kegiatan = self.get_kegiatan(id)
+        kegiatan.unarchived()
+        return Response({'status': 'Kegiatan unarchived successfully'}, status=status.HTTP_200_OK)
+
+    def get_kegiatan(self, id):
+        try:
+            return Acara.objects.get(id=id)
+        except Acara.DoesNotExist:
+            try:
+                return Aktivitas.objects.get(id=id)
+            except Aktivitas.DoesNotExist:
+                raise NotFound(f"Kegiatan dengan id {id} tidak ditemukan", code="Kegiatan_Not_Found")
+
+class DeleteKegiatanView(DestroyAPIView):
+    permission_classes = [IsAuthenticated, IsCurrentUserOrReadOnly]
+
+    def delete(self, request, id):
+        instance = self.get_object(id)
+        self.perform_destroy(instance)
+        return Response({'status': 'Kegiatan deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+    def get_object(self, id):
+        try:
+            return Acara.objects.get(id=id)
+        except Acara.DoesNotExist:
+            try:
+                return Aktivitas.objects.get(id=id)
+            except Aktivitas.DoesNotExist:
+                raise NotFound(f"Kegiatan dengan id {id} tidak ditemukan", code="Kegiatan_Not_Found")
